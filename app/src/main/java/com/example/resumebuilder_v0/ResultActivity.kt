e5bc361adecaf96434db8449e7b1f74ba4d9f06d
younis <yereiqat@gmail.com>
2021-05-15 14:58:41 -0700

    touching up, need to finish pdf generation.

--- app/src/main/java/com/example/resumebuilder_v0/ResultActivity.kt
+++ app/src/main/java/com/example/resumebuilder_v0/ResultActivity.kt
@@ -1,32 +1,60 @@
 package com.example.resumebuilder_v0
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.pdf.PdfDocument
+import android.graphics.pdf.PdfDocument.PageInfo
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
-import com.example.resumebuilder_v0.databinding.ActivityResultBinding
-import java.io.Serializable
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.internal.ViewUtils.getContentView
+import java.io.OutputStream
+
 
 class ResultActivity : AppCompatActivity() {
+    @SuppressLint("RestrictedApi", "SetTextI18n")
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
-
-
-
-
-        //gets info from previous activity, setup download button
         var info  = intent.getSerializableExtra("ResumeInfo") as? ScrollingActivity.ResumeInfo
+
+
+        /*
+        val resumePDF = PdfDocument()
+        val pageInfo = PageInfo.Builder(100, 100, 1).create()
+        var page = resumePDF.startPage(pageInfo)
+        val content = this.findViewById<View>(android.R.id.content)
+        content.draw(page.canvas)
+        resumePDF.finishPage(page)
+        resumePDF.writeTo()
+        resumePDF.close()
+         */
+
+
+
+        //setup download button
         val downloadButton = findViewById<Button>(R.id.downloadButton)
         downloadButton.setOnClickListener{
+
+            var testText = findViewById<TextView>(R.id.resultText)
+            testText.text = info!!.name + info.education + info.major + info.work + info.contact
+            /*
             showToast(info!!.name)
             showToast(info.education)
             showToast(info.major)
             showToast(info.work)
             showToast(info.contact)
+             */
         }
+
+
 
 
         //button to return to original screen
